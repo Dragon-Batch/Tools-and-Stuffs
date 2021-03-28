cdd7dceaa37d3f2dfdd87b8770a70e42bc447d36
Dragon-Batch <72404601+Dragon-Batch@users.noreply.github.com>
2021-03-28 05:27:49 -0700

    Delete Extreme Fast Pinger.bat

--- Extreme Fast Pinger.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-set /p "IP=IP : "
-
-:Pinger
-for /f "tokens=1-6 delims= " %%a in ('start /b Ping -n 1 -w 1 -l 1 %IP% ^| find ^"Reply^"') do (
-    echo [38;2;255;0;0m%%a [38;2;0;0;255m%%b [38;2;128;0;128m%%c [38;2;255;192;203m%%d [38;2;0;255;255m%%e [38;2;128;128;128m%%f
-)
-goto Pinger
